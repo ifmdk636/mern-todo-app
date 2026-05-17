d96be7af0b9ad1facd40b53c91b29d40c7cd4242
Ibrahim Faris <ibrahimfaris636@gmail.com>
2026-05-17 10:50:20 +0700

    ready to deploy

--- backend/connection/connection.js
+++ backend/connection/connection.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config({ path: "../../.env" });
+
+const mongoURI = process.env.MONGO_URI;
+console.log(process.env.MONGO_URI);
 const conn = async () => {
   try {
-    await mongoose.connect(
-      "mongodb://admin:admin123@ac-yelefp6-shard-00-00.yhszbsm.mongodb.net:27017,ac-yelefp6-shard-00-01.yhszbsm.mongodb.net:27017,ac-yelefp6-shard-00-02.yhszbsm.mongodb.net:27017/?ssl=true&replicaSet=atlas-297b5z-shard-0&authSource=admin&appName=Ibrahim",
-    );
+    await mongoose.connect(mongoURI);
+
     console.log("Connected");
   } catch (error) {
     console.log(error);
